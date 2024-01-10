c03a0d517bcd5a6247cf92087449e369506e58aa
redwan <redtah2007@yahoo.com>
2024-01-10 21:40:31 +0300

    Add javascript file

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Navigation bar eddects on scroll
+window.addEventListener("scroll", function(){
+    const header=this.document.querySelector("header");
+    header.classList.toggle("sticky" , window.scrollY>0)
+});
