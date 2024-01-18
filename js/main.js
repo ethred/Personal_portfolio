003c6f5ece8da030126cda8bbff64c9027a7081a
redwan <redtah2007@yahoo.com>
2024-01-18 09:50:16 +0300

    add javascript for contactus

--- js/main.js
+++ js/main.js
@@ -28,3 +28,44 @@ modalCloseBtns.forEach((modalCloseBtn)=>{
         });
     });
 }) ;
+
+// portfolio Section 
+const portfolioModals= document.querySelectorAll(".portfolio-model");
+const imgCards= document.querySelectorAll(".img-card");
+const portfolioCloseBtns= document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function(modalClick){
+    console.log('iam here')
+    portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) =>{
+    imgCard.addEventListener("click",()=>{
+        portfolioModal(i);
+    });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn)=>{
+    portfolioCloseBtn.addEventListener("click", () => {
+        portfolioModals.forEach((portfolioModalView)=>{
+            portfolioModalView.classList.remove("active");
+        });
+    });
+}) ;
+
+
+// our clinets - Swiper
+
+var swiper = new Swiper(".client-swiper", {
+    slidesPreView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination : {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
