7042dba4e5aa29039be16fca6a23784a6a7af397
redwan <redtah2007@yahoo.com>
2024-01-19 16:54:31 +0300

    Add click event to the top scroll button

--- js/main.js
+++ js/main.js
@@ -79,3 +79,8 @@ const scrollTopBtn = document.querySelector(".scrollToTop-btn");
 window.addEventListener("scroll", function(){
  scrollTopBtn.classList.toggle("active", window.scrollY > 500);
 });
+
+scrollTopBtn.addEventListener("click", ()=>{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop= 0;
+})
